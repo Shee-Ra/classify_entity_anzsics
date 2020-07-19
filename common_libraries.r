063efad09b50e6f2518743bd8e1ca0ea6a6438bf
Dr Mac <shee.ra.codes@gmail.com>
2020-07-20 09:45:30 +1000

    added common  library script

---
+++ common_libraries.r
@@ -0,0 +1,16 @@
+# common libraries
+
+library(pacman)
+p_load(cutpointr # metric / evaluation
+       , fst
+       , ggplot2
+       , here
+       , janitor
+       , magrittr
+       , quanteda  # bag o' words
+       , rsample   # sampling
+       , ranger    # model
+       , tictoc    # timing
+       , tidyverse
+       , XML
+       )
